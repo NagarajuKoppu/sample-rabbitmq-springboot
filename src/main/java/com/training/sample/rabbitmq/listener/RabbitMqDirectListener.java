4a3b17c29795f5571c443ad9309869bd3f1aeec4
Nagaraju_Koppu <Nagaraju_Koppu@EPINHYDW0230.princeton.epam.com>
2020-04-16 16:29:07 +0530

    manual ack on @RabbitListener

--- src/main/java/com/training/sample/rabbitmq/listener/RabbitMqDirectListener.java
+++ src/main/java/com/training/sample/rabbitmq/listener/RabbitMqDirectListener.java
@@ -1,8 +1,13 @@
 package com.training.sample.rabbitmq.listener;
 
+import java.io.IOException;
+
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
+import com.rabbitmq.client.Channel;
 import com.training.sample.rabbitmq.dto.Employee;
 
 /**
@@ -13,8 +18,10 @@ import com.training.sample.rabbitmq.dto.Employee;
 public class RabbitMqDirectListener {
 
 	@RabbitListener(queues = "${app.mq.direct.queuename}")
-	public void onMessage(Employee emp) {
+	public void onMessage(Employee emp, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long deliveryTag)
+			throws IOException {
 		System.out.println("--------------Received message-----------");
 		System.out.println(emp);
+		channel.basicAck(deliveryTag, false);
 	}
 }
